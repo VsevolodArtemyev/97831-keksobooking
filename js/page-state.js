966e61364135e17f590c05006ce9915006f5daad
Asenchik Anastasiya <n-asenchik@yandex.ru>
2018-02-23 23:18:15 +0300

    add loading files

--- js/page-state.js
+++ js/page-state.js
@@ -8,6 +8,10 @@
   var noticeForm = document.querySelector('.notice__form');
   var fieldsForm = document.querySelectorAll('fieldset');
   var fieldAddress = document.querySelector('#address');
+  var mapCard = document.querySelector('.map__card');
+  var photoContainer = document.querySelector('.form__photo-container .upload');
+  var avatar = document.querySelector('.notice__preview img');
+  var defaultAvatarSrc = document.querySelector('.notice__preview img').src;
 
   var renderedPins = [];
 
@@ -24,15 +28,24 @@
       disableFields(false);
     },
     setPassive: function () {
+      var uploadedPhotos = document.querySelectorAll('.uploaded-photo');
+
       mainPin.style = '';
       mainPin.style.transform = 'translate(0,0)';
-      var mapCard = document.querySelector('.map__card');
 
       if (mapCard) {
         var pinOfCard = map.querySelector('.map__card + .map__pin');
         map.removeChild(pinOfCard);
         map.removeChild(mapCard);
       }
+
+      if (uploadedPhotos) {
+        [].forEach.call(uploadedPhotos, function (photo) {
+          photoContainer.removeChild(photo);
+        });
+      }
+
+      avatar.src = defaultAvatarSrc;
 
       renderedPins = mapPins.querySelectorAll('.map__pin');
       [].forEach.call(renderedPins, function (pin) {
---
+++ js/upload-photos.js
@@ -0,0 +1,35 @@
+'use strict';
+
+(function () {
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+
+  var fileChooser = document.querySelector('#images');
+  var photoContainer = document.querySelector('.form__photo-container .upload');
+
+  var templatePhoto = document.createElement('img');
+  templatePhoto.classList.add('uploaded-photo');
+  templatePhoto.height = 70;
+  templatePhoto.style = 'display: inline-block; margin-right: 10px;';
+
+  fileChooser.addEventListener('change', function () {
+    var files = fileChooser.files;
+
+    for (var i = 0; i < files.length; i++) {
+      var fileName = files[i].name.toLowerCase();
+      var matches = FILE_TYPES.some(function (elem) {
+        return fileName.endsWith(elem);
+      });
+
+      if (matches) {
+        var reader = new FileReader();
+
+        reader.addEventListener('load', function () {
+          var newPhoto = templatePhoto.cloneNode();
+          newPhoto.src = reader.result;
+          photoContainer.insertBefore(newPhoto, photoContainer.firstChild);
+        });
+        reader.readAsDataURL(files[i]);
+      }
+    }
+  });
+})();
